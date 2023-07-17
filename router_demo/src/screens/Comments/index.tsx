77d31d5783c1bb2b5f9f76743b0d627aa17dd2a2
TrungNT <trungnt10012k@gmail.com>
2023-07-17 18:49:50 +0700

    initilize commit

--- router_demo/src/screens/Comments/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom"
-const Comments = () => {
-    const { postId } = useParams();
-    return <h1>Display all comments of post number {postId}</h1>
-}
-
-export default Comments;
--- router_demo/src/screens/Posts/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const Posts = () => {
-    return <h1>Display all post</h1>
-}
-
-export default Posts;
