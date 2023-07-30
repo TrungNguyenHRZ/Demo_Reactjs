190ef1620e6b3f8fdf885272ff397f64b40bf3ce
TranThucKhue <thuckhue2907@gmail.com>
2023-07-30 18:11:47 +0700

    update welcome page

--- src/screen/welcome/index.tsx
+++ src/screen/welcome/index.tsx
@@ -1,6 +1,66 @@
-const Welcome=()=>{
-    return(
-        <div>Welcome</div>
-    )
+import React, { useState } from 'react';
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
+import './index.css';
+import Button from 'react-bootstrap/Button';
+
+interface FormData {
+  username: string;
+  gender: string;
 }
-export default Welcome;
+
+const UserForm = () => {
+  const initialValues: FormData = {
+    username: '',
+    gender: '',
+  };
+
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().required('Username is required!'),
+    gender: Yup.string().required('Gender is required!'),
+  });
+
+  const handleSubmit = (values: FormData) => {
+    console.log('Form data submitted:', values);
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+      
+    >
+      <Form className='form'>
+      <h1 className='header'>Welcome to The Start Wars</h1>
+      <div className='container'>
+        <div>
+          <label>
+            Username:
+            <Field type="text" name="username" />
+            <ErrorMessage name="username" component="div" className="error" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Gender:
+            <Field as="select" name="gender">
+              <option value="">Select gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </Field>
+            <ErrorMessage name="gender" component="div" className="error" />
+          </label>
+        </div>
+        <div>
+        <Button type='submit' variant="danger">Submit</Button>{' '}
+        </div>
+      </div>
+        
+      </Form>
+    </Formik>
+  );
+};
+
+export default UserForm;
